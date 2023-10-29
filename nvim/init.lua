21d52a7747455c6dc885ab1393742d8710cee9b3
hideyuki-hori <montecarlo1101@gmail.com>
2023-10-30 08:34:32 +0900

    refactor(nvim/init): Manually load configs using stdpath
    Switched to manual loading to ensure consistent behavior across different environments and to simplify the config loading process.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,13 @@
 vim.cmd('autocmd!')
 
-local init_path = vim.fn.expand('%:p')
-local lua_path = vim.fn.fnamemodify(init_path, ':h') .. '/'
+local script_path = vim.fn.stdpath('config') .. '/'
+package.path = script_path .. "?.lua;" .. package.path
 
-local files = vim.fn.globpath(lua_path, '*.lua', false, true)
-files = vim.tbl_filter(function (file)
-  return not file:match('init.lua$')
-end, files)
+require('encoding')
+require('editing')
+require('indentation')
+require('keymap')
+require('plugins')
+require('system')
+require('ui')
 
-for _, file in ipairs(files) do
-  local module = file:sub(#lua_path + 1, -5):gsub('/', '.')
-  require(module)
-end
