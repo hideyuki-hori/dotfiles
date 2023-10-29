7e8591796f06a16125ea0df28964a0431d8021e0
hideyuki-hori <montecarlo1101@gmail.com>
2023-10-29 11:48:31 +0900

    feat(nvim/ui): Enhance UI settings and yank highlight
    - Enable cursor line highlighting.
    - Activate true color support in terminal.
    - Set window blending level.
    - Use popup menu for command-line completion.
    - Adjust popup menu blending for better visibility.
    - Set editor background to dark.
    - Highlight yanked text with a visual group for a short duration.

--- nvim/ui.lua
+++ nvim/ui.lua
@@ -10,6 +10,18 @@ vim.opt.scrolloff = 10
 vim.opt.inccommand = 'split'
 vim.opt.breakindent = true
 vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
 
 vim.cmd([[let &t_Cs = '\e[4:3m']])
 vim.cmd([[let &t_Ce = '\e[4:0m']])
+vim.cmd [[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  augroup END
+]]
