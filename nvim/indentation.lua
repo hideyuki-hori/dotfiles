7cc47d29a4f091bc0a778722b36445b7f49ecb39
hideyuki-hori <montecarlo1101@gmail.com>
2023-12-22 08:55:31 +0900

    feat(nvim): Configure markdown indentation in indentation.lua
    - Added Lua script to set indentation to 2 spaces for Markdown files.
    - Adjustments made for consistent code style and improved readability in Markdown editing.
    - Configuration included in the 'indentation.lua' file.

--- nvim/indentation.lua
+++ nvim/indentation.lua
@@ -4,3 +4,13 @@ vim.opt.expandtab = true
 vim.opt.smarttab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
