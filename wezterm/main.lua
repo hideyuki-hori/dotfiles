c5824502e2809d89d5370e55be9a20ba0fea20a1
hideyuki-hori <montecarlo1101@gmail.com>
2025-04-06 16:09:34 +0900

    feat(wezterm): Add color theme

--- wezterm/main.lua
+++ wezterm/main.lua
@@ -1,6 +1,8 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.color_scheme = 'Edge Dark (base16)'
+
 config.font = wezterm.font 'Myrica M'
 config.font_size = 16.0
 
