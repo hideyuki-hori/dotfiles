0bca8756abaceaca627a4dd78c31307a7abd5e9e
hideyuki-hori <montecarlo1101@gmail.com>
2025-04-06 19:57:16 +0900

    feat(wezterm): Add control multi pane

--- wezterm/main.lua
+++ wezterm/main.lua
@@ -8,4 +8,38 @@ config.font_size = 16.0
 
 config.hide_tab_bar_if_only_one_tab = true
 
+config.keys = {
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action_callback(function(window, pane)
+      local tab = window:active_tab()
+      if #tab:panes() > 1 then
+        window:perform_action(
+          wezterm.action.CloseCurrentPane { confirm = false },
+          pane
+        )
+      else
+        window:toast_notification("WezTerm", "Cannot close the last pane", nil, 2000)
+      end
+    end),
+  },
+  {
+    key = 'w',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+}
+
 return config
+
